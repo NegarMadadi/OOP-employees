c76fc0c1c1c71f55a82b40622282414b615c76ae
NegarMadadi <negar.myeah@gmail.com>
2023-06-15 13:24:53 +0200

    List.contains() and Object.equals()

--- src/main/java/org/example/employees/Programmer.java
+++ src/main/java/org/example/employees/Programmer.java
@@ -1,5 +1,6 @@
 package org.example.employees;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,6 +48,18 @@ public class Programmer extends Employee {
 
     public void setIq(int iq) {
         this.iq = iq;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Programmer that)) return false;
+        if (!super.equals(o)) return false;
+        return iq == that.iq;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), iq);
     }
 }
 
